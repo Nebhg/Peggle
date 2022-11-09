0bacf416684eed7b49f01484bf180b915c8b4ede
Yernemm <yrnemmizer@gmail.com>
2022-11-09 19:57:14 +0000

    Limit to one ball on screen

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -8,10 +8,13 @@ public class Shoot : MonoBehaviour
     public float LaunchForce;
     public GameObject Pinball;
 
+    public bool canFire {get; set;}
+
     // Start is called before the first frame update
     void Start()
     {
         ammo = 10;
+        canFire = true;
     }
 
     // Update is called once per frame
@@ -26,11 +29,14 @@ public class Shoot : MonoBehaviour
 
     void Fire()
     {
-        if(ammo > 0)
+        if(ammo > 0 && canFire)
         {
             GameObject PinballIns = Instantiate(Pinball,transform.position,transform.rotation);
             PinballIns.GetComponent<Rigidbody2D>().velocity = transform.up * LaunchForce;
+            PinballIns.GetComponent<Ball>().shooter = this;
             ammo--;
+
+            canFire = false;
         }
         Debug.Log("You have " + ammo + " remaining!");
         //.AddForce(transform.up * LaunchForce);
