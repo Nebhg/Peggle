87c168f4c4250765bebb03aaa343e5396626bc12
Yernemm <yrnemmizer@gmail.com>
2022-11-29 17:01:30 +0000

    Optimise Y checks

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -7,23 +7,34 @@ public class Ball : MonoBehaviour
 
     public Shoot shooter {get; set;}
 
+    private Powerup powerup;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.powerup = new PowerupNone();
+
+        StartCoroutine(checkHeight);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.y < 0){
-            shooter.canFire = true;
-            Destroy(gameObject);
-            if(Shoot.ammo <= 0){
-                Debug.Log("Out of ammo!");
-                GameController.GameLose();
-            }
-        }
+
     }
+
+    IEnumerator checkHeight(){
+        while(gameObject.transform.position.y > 0){
+            yield return new WaitForSeconds(0.5f);
+        }
+        //Ball below y=0
+        shooter.canFire = true;
+        Destroy(gameObject);
+        if(Shoot.ammo <= 0){
+            GameController.GameLose(GameController.LossReason.OutOfAmmo);
+        }
+
+    }
+
 
 }
