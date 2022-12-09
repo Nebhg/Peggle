0fac11db2ac883fd93c56717007c12d95887106d
Yernemm <yrnemmizer@gmail.com>
2022-12-09 05:29:56 +0000

    shoot with mouse and fix lighting

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -26,7 +26,7 @@ public class Shoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             Fire();
         }
